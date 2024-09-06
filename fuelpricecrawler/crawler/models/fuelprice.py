a28a22ad55a166c624501cc9a906c628c6fa0b6d
sailesh kumar <skumar@petz.net>
2024-09-07 00:58:23 +0530

    rename field

--- fuelpricecrawler/crawler/models/fuelprice.py
+++ fuelpricecrawler/crawler/models/fuelprice.py
@@ -11,13 +11,13 @@ class FuelPrice(models.Model):
     state = models.CharField(max_length=255)
     fuel = models.CharField(max_length=50, default='petrol', choices=FUEL_CHOICES)
     price = models.DecimalField(max_digits=7, decimal_places=2)
-    created_at = models.DateField(auto_now_add=True)
+    date = models.DateField(auto_now_add=True)
     
     class Meta:
-        ordering = ["state", "city"]
+        ordering = ["-date"]
         verbose_name = "Fuel Price"
         verbose_name_plural = "Fuel Prices"
-        unique_together = ["state", "city", "created_at"]
+        unique_together = ["state", "city", "date"]
     
     def __str__(self):
         return f"{self.state}| {self.city} (Rs. {self.fuel})"
