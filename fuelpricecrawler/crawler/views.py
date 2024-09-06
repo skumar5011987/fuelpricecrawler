444d16029096ab1539fa0767bb98158d8ef9d731
sailesh kumar <skumar@petz.net>
2024-09-07 00:31:14 +0530

    home url and view

--- fuelpricecrawler/crawler/views.py
+++ fuelpricecrawler/crawler/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from fuelpricecrawler.views import (BaseAPIView, APIResponse ,SUCCESS, FAIL,)
 
-# Create your views here.
+
+class HomeAPIView(BaseAPIView):
+    def get(self, request):
+        return APIResponse(SUCCESS, message="Welcome, Fuel price crawler is ready to work.")
