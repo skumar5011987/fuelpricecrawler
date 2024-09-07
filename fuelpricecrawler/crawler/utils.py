e0076f7d22cdf97758605788784ee0904a57cbd2
sailesh kumar <skumar@petz.net>
2024-09-08 00:09:22 +0530

    added utitlity functions

---
+++ fuelpricecrawler/crawler/utils.py
@@ -0,0 +1,12 @@
+from decimal import Decimal
+from datetime import datetime
+
+def parse_date(date_str=""):
+    if date_str:
+        date_obj = datetime.strptime(date_str, '%b %d, %Y').date()
+        return date_obj
+
+def parse_price(price=""):
+    p = price.split(" ")[0]
+    return Decimal(p)
+    pass
