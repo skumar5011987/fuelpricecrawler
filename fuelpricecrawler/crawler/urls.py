630690fefc0c6adb4e60c4fe48876cd47aaf786f
sailesh kumar <skumar@petz.net>
2024-09-08 22:21:39 +0530

    update requirements and comment url

--- fuelpricecrawler/crawler/urls.py
+++ fuelpricecrawler/crawler/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, re_path
-from crawler.views import (HomeAPIView, CrawlAPIView, ListCitiesAPIView, FuelPricesAPIView)
+from crawler.views import (HomeAPIView, ListCitiesAPIView, FuelPricesAPIView)
 
 urlpatterns = [
     path("", HomeAPIView.as_view(), name="home"),
     path("list-cities", ListCitiesAPIView.as_view(), name="city list"),
-    path("crawl-fuel-prices", CrawlAPIView.as_view(), name="crawl fuel prices"),
     path("fuel-price", FuelPricesAPIView.as_view(), name="fuel prices"),
+    # path("crawl-fuel-prices", CrawlAPIView.as_view(), name="crawl fuel prices"),
 ]
