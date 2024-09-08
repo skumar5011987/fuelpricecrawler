51e60157d67724593ffff458ff03bc10c18d3561
sailesh kumar <skumar@petz.net>
2024-09-08 11:50:29 +0530

    celery method to scedule to crawl a page

--- fuelpricecrawler/fuelpricecrawler/celery.py
+++ fuelpricecrawler/fuelpricecrawler/celery.py
@@ -1,15 +1,63 @@
 
 import os
-from celery import Celery, shared_task
+import time
+from django.conf import settings
+from celery import Celery
+from celery.signals import task_failure
+from celery.utils.log import get_task_logger
+
+from crawler.scripts.ndtv_crawler import (
+    parse_page_url, extract_fuelprice_history, get_page_content
+    )
+from crawler.models.fuelprice import Location
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fuelpricecrawler.settings')
+_logger = get_task_logger(__name__)
 
 app = Celery('fuelpricecrawler')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+@task_failure.connect
+def handle_task_failure(**kwargs):
+    from traceback import format_tb
+
+    _logger.error(
+        "[task:%s:%s]"
+        % (
+            kwargs.get("task_id"),
+            kwargs["sender"].request.correlation_id,
+        )
+        + "\n"
+        + "".join(format_tb(kwargs.get("traceback", [])))
+        + "\n"
+        + str(kwargs.get("exception", ""))
+    )
 
 
+@app.task(name="crawl_fuelprices", max_retries=3, retry_backoff=True, rate_limit="300/m")
+def crwal_fuelprices(city="", page_url=""):
+    
+    if not (city and page_url):
+        print(f"City & Page url is required.")
+        return
+    
+    try:
+        if not page_url:
+            url = parse_page_url(city)
+        
+        print(f"[Info]: Crawling '{url}'")
+        time.sleep(1)
+        content = get_page_content(url)
+        
+        data = extract_fuelprice_history(city, content)
+        Location.create_data(data)
+    except Exception as exc:
+        _logger.exception(exc)
+        crwal_fuelprices.apply_async(city, url, countdown=10, exc=exc)
+    
 
